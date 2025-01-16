2b0065d025c2683632b1cac79b8d12c92756b541
Nicotnn <tananow.nico@gmx.de>
2025-01-16 15:46:13 +0100

    pylightning

--- src/chest_xray_diagnosis/model.py
+++ src/chest_xray_diagnosis/model.py
@@ -1,33 +1,50 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+import pytorch_lightning as pl
 
-
-class CNN_Baseline(nn.Module):
-    def __init__(self,num_classes=2):
-        super(CNN_Baseline,self).__init__()
-        self.conv1 = nn.Conv2d(3,16, kernel_size=3,padding=1)
-
-        self.conv2 = nn.Conv2d(16,32,kernel_size=3,padding=1)
+class CNN_Baseline(pl.LightningModule):
+    """Convolutional Neural Network base mode."""
+    def __init__(self) -> None:
+        super().__init__()
+        self.conv1 = nn.Conv2d(3, 16, 3, 1)
+        self.conv2 = nn.Conv2d(16, 32, 3, 1)
+        self.dropout = nn.Dropout(0.5)
+        self.fc1 = nn.Linear(32 * 32 * 32, 2)
         
-        self.fc1 = nn.Linear(32*32*32, num_classes)
-
-    def forward(self,x):
-        x = F.relu(self.conv1(x))
-        x = F.max_pool2d(x,2)
-
-        x = F.relu(self.conv2(x))
-        x = F.max_pool2d(x,2)
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        """Forward pass."""
+        x = torch.relu(self.conv1(x))
+        x = torch.max_pool2d(x, 2, 2)
+        x = torch.relu(self.conv2(x))
+        x = torch.max_pool2d(x, 2, 2)
         
+        x = torch.flatten(x, 1)
+        x = self.dropout(x)
+        return self.fc1(x)
+    
+    def training_step(self, batch):
+        """Define training step for Lightning"""
+        return None
 
-        x = x.view(x.size(0),-1)
-
-        x = self.fc1(x)
-
-        return x
-
+    #    img, target = batch
+    #    y_pred = self(img)
+    #    return self.loss_fn(y_pred, target)
+    
+    def configure_optimizers(self):
+        """Define optimizer for Lightning"""
+        return None
+    #   return torch.optim.Adam(self.parameters(), lr=1e-3)
+    
     def save(self, filename):
         torch.save(self.state_dict(), filename)
 
     def load(self, filename):
         self.load_state_dict(torch.load(filename))
+        
+if __name__ == "__main__":
+    
+    #trainer = pl.Trainer(fast_dev_run=True)
+    
+    cnn = CNN_Baseline()
+    print(cnn)
