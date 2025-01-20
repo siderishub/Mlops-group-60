20b1f91060dfa1784827c089221a75c29fdf12cf
Nicotnn <tananow.nico@gmx.de>
2025-01-20 19:55:53 +0100

    added more features to api

--- src/chest_xray_diagnosis/frontend.py
+++ src/chest_xray_diagnosis/frontend.py
@@ -1,14 +1,16 @@
 import streamlit as st
 from PIL import Image
 
+# to run quickly use "streamlit run frontend.py"
+
 img_size = 128
 
-st.title(':robot_face:Pneumonia Detection Model using timm')
+st.title(':robot_face: Pneumonia Detection Model using timm')
 
-st.info('This app can be used to detect pneumonia from chest X-ray images')
+st.info('This app can be used to detect pneumonia from chest X-ray images, just drop the X-Ray image below')
 
 
-img_data = st.file_uploader(label='load the image', type=['png', 'jpg', 'jpeg'])
+img_data = st.file_uploader(label='imageloader', type=['png', 'jpg', 'jpeg'])
 
 if img_data is not None:
     
@@ -19,13 +21,18 @@ if img_data is not None:
     
     # load image file for prediction
     
-    img_path = f'./{img_data.name}'
-    img = img.load_imag(img_path), target_size=(img_size, img_size)
+    #img_path = f'./{img_data.name}'
+    #img = img.load_imag(img_path), target_size=(img_size, img_size)
     
     prediction = None # Make model.predict(img)
     
-st.title('Our (very smart) model carefully examined the X-Ray and the likely diagnosis is:')
-st.write(prediction)
+prediction = None 
+if prediction == None:
+    st.write('Please upload an image to get the prediction!')
+    
+if prediction is not None:
+    st.write('Our (very smart) model carefully examined the X-Ray and the likely diagnosis is:')
+    st.write(prediction)
 
 if prediction == 0:
     st.write('Lucky you, everything seems fine!')
