d16e5665c89984015881d3b742f05bb18825f64b
ThomasRoennebek <hansen.fodbold@live.dk>
2025-01-22 14:15:02 +0100

    added locustfile.py

---
+++ tests/performancetests/locustfile.py
@@ -0,0 +1,14 @@
+import random
+
+from locust import HttpUser, between, task
+
+
+class MyUser(HttpUser):
+    """A simple Locust user class that defines the tasks to be performed by the users."""
+
+    wait_time = between(1, 2)
+
+    @task
+    def get_root(self) -> None:
+        """A task that simulates a user visiting the root URL of the FastAPI app."""
+        self.client.get("/")
